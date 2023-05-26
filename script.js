b2488fce30a65eda69e46f7569b43c6c25836be8
Vaibhav Meena <vaibhavmeena91@gmail.com>
2023-05-26 18:05:50 +0530

    Completed Javascript and CSS files for the project.

--- script.js
+++ script.js
@@ -1,84 +1,137 @@
-let choices = ['rock', 'paper', 'scissors'];
+const choices = ['rock', 'paper', 'scissors'];
+
+let mainStatus = document.querySelector('#mainStatus');
+let subStatus = document.querySelector('#subStatus');
+let rock = document.getElementById('rock');
+let paper = document.getElementById('paper');
+let scissors = document.getElementById('scissors');
+let playerScore = document.getElementById('playerScore');
+let computerScore = document.getElementById('computerScore');
+let playerMoveImage = document.getElementById('playerMoveImage');
+let ComputerMoveImage = document.getElementById('ComputerMoveImage');
+let modal = document.getElementById('modal');
+let resetButton = document.getElementById('reset');
+let modalText = document.getElementById('modal-text');
+
+
 
 function getComputerChoice() {
-   return choices[Math.floor( Math.random()  * choices.length ) ];
+   return choices[Math.floor( Math.random()  * 3) ];
 }
-
 
 let winCount = 0;
 let lostCount = 0;
-let tieCount = 0;
-
-function playerSelection() {
-    let userInput = prompt(" rock , paper or scissors ?");
-    return userInput.toLowerCase();
-}
 
 
-while( (winCount+lostCount + tieCount) != 5)
+function game(x) {
+    let y = x;
+    let z = getComputerChoice();
 
-{ 
+    if(lostCount >= 5 || winCount >= 5) {
 
-    function game() {
-        let y =  playerSelection();
-        let z = getComputerChoice();
-     
+        if(lostCount < winCount ) {
+            modalText.textContent = "You won!";
+    
+        } else if(lostCount > winCount ) {
+            modalText.textContent = "You lost...";
+            
+        } 
+
+        showWinnerModal();
+        
+    }
+    else{
         if (y === z ) {
-         alert(`it\'s a tie `);
-         tieCount++;
-        }
-     
-        else if (y === 'rock'  && z=== 'paper' ) {
-         alert(`You lost! ${y}  to  ${z} `);
-         lostCount++;
-     
-        }   
-     
-        else if (y === 'rock'  && z=== 'scissors' ) {
-         alert(`hurray you won ${y}  beats  ${z} `);
-         winCount++;
+            mainStatus.textContent = `It's a tie!`;
+            subStatus.textContent = `${y} ties with ${z}`;
+        } else if ((y === 'rock' && z === 'paper') || (y === 'scissors' && z === 'rock') || (y === 'paper' && z === 'scissors')) {
+            mainStatus.textContent = `You lost!`;
+            subStatus.textContent = `${y} is beaten by ${z}`;
+            lostCount++;
+        } else if ((y === 'rock' && z === 'scissors') || (y === 'scissors' && z === 'paper') || (y === 'paper' && z === 'rock')) {
+            mainStatus.textContent = `You won!`;
+            subStatus.textContent = `${y} beats ${z}`;
+            winCount++;
         } 
-     
-        else if (y === 'scissors'  && z=== 'paper' ) {
-         alert(`hurray you won ${y} beats  ${z} `);
-         winCount++;
-        } 
-     
-        else if (y === 'scissors'  && z=== 'rock' ) {
-         alert(`You lost! ${y}  to  ${z} `);
-         lostCount++;
-        } 
-     
-        else if (y === 'paper'  && z=== 'scissors' ) {
-         alert(`You lost!  ${y}  to  ${z} `);
-         lostCount++;
-        } 
-     
-        else if (y === 'paper'  && z=== 'rock' ) {
-         alert(`hurray you won ${y}  beats ${z} `);
-         winCount++;
-        } 
-     
-        else {
-         alert("Please enter a valid option");
-        }
-     
-     }
-     
+        else 
+        {
+            mainStatus.textContent =  "Please enter a valid option";
+        }    
+    }
+    
+    
+    ComputerMoveImage.textContent = z;
+    playerScore.textContent = `Player: ${winCount} ` ;
+    computerScore.textContent = `Computer: ${lostCount} `;
+
+    switch (y) {
+        case 'rock':
+            playerMoveImage.textContent = `✊`;
+            break;
+        case 'paper':
+            playerMoveImage.textContent = `✋`;
+            break;
+        case 'scissors':
+            playerMoveImage.textContent = `✌️`;
+            break;
+    }
+
+    switch (z) {
+        case 'rock':
+            ComputerMoveImage.textContent = `✊`;
+            break;
+        case 'paper':
+            ComputerMoveImage.textContent = `✋`;
+            break;
+        case 'scissors':
+            ComputerMoveImage.textContent = `✌️`;
+            break;
+    }
+
+
+
+
 
 }
 
- 
+
+rock.addEventListener('click', () => game('rock'))
+paper.addEventListener('click', () => game('paper'))
+scissors.addEventListener('click', () => game('scissors'))
 
 
 
-if(lostCount < winCount ) {
-    alert(`you won! by ${winCount} - ${lostCount} `)
+if(lostCount >= 5 || winCount >= 5) {
+
+    if(lostCount < winCount ) {
+        modalText.textContent = "You won the game!";
+
+    } else if(lostCount > winCount ) {
+        modalText.textContent = "You lost the game!";
+        
+    } else {
+        modalText.textContent = "It's a tie!";
+        
+    }
+    
 }
-else if(lostCount > winCount ) {
-    alert(`you lost! by ${winCount} - ${lostCount} `)
+
+
+function showWinnerModal() {
+    modal.classList.remove('hidden'); // make the modal visible
+    setTimeout(function() {
+        modal.classList.remove('scale-0', 'opacity-0'); // start the transition
+    }, 0); // no delay, but using setTimeout to ensure this happens in the next event loop
 }
 
-else {
-    alert("it's a tie")
-}
+resetButton.addEventListener('click', function() {
+    modal.classList.add('scale-0', 'opacity-0'); // hide the modal
+    setTimeout(() => modal.classList.add('hidden'), 500); // delay hiding to allow transition to complete
+    winCount = 0;
+    lostCount = 0;
+    playerScore.textContent = `Player: ${winCount} ` ;
+    computerScore.textContent = `Computer: ${lostCount} `;
+
+    // hide the modal when the button is clicked
+});
+
